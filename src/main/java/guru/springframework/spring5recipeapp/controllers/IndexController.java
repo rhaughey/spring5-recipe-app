1cb15199917efefeeea5ca828e9f9027666d9335
rhaughey <38265819+rhaughey@users.noreply.github.com>
2018-06-24 14:33:32 -0400

    Adding Bootstrap & Service classes

--- src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
@@ -1,14 +1,23 @@
 package guru.springframework.spring5recipeapp.controllers;
 
+import guru.springframework.spring5recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+    private final RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
     @RequestMapping({"","/","/index"})
-    public String getIndexPage(){
-        System.out.println("Get on your feet and do the Funky Alfonzo!");
+    public String getIndexPage(Model model){
+
+        model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
 }
